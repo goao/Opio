d5284ac7b80a1583e7edfb291ed3a6b97b77116f
Mario Caseiro <mario_contato@yahoo.com.br>
2011-03-04 16:38:10 -0300

    update

--- loto1/src/util/GraphQtdByTime.java
+++ loto1/src/util/GraphQtdByTime.java
@@ -1,14 +1,32 @@
 package util;
 import java.awt.Color;
+import java.awt.Font;
+import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import javax.swing.AbstractAction;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+
+import org.apache.batik.dom.GenericDOMImplementation;
+import org.apache.batik.svggen.SVGGraphics2D;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.StandardLegend;
+import org.jfree.chart.labels.StandardXYToolTipGenerator;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.chart.plot.XYPlot;
 import org.jfree.chart.renderer.XYLineAndShapeRenderer;
@@ -17,7 +35,8 @@ import org.jfree.data.XYSeries;
 import org.jfree.data.XYSeriesCollection;
 import org.jfree.ui.ApplicationFrame;
 import org.jfree.ui.RefineryUtilities;
-import org.jfree.ui.Spacer;
+import org.w3c.dom.DOMImplementation;
+import org.w3c.dom.Document;
 
 import softonPack.util.DateHandle;
 
@@ -27,6 +46,7 @@ import com.mysql.jdbc.Statement;
 public class GraphQtdByTime extends ApplicationFrame {
 
 	private Connection conn;
+	private AbstractAction exportFigure;
 
 	/**
 	 * Creates a new demo.
@@ -61,8 +81,68 @@ public class GraphQtdByTime extends ApplicationFrame {
 		chartPanel.setPreferredSize(new java.awt.Dimension(500, 270));
 		setContentPane(chartPanel);
 		chartPanel.setMouseZoomable(true, false);
+		
+        exportFigure = new AbstractAction("Export figure") {
+            public void actionPerformed(ActionEvent arg0) {
+                exportFigure(chart);
+            }
+        };
+
+        this.setJMenuBar(createMenuBar());
+
 
 	}
+	
+    private JMenuBar createMenuBar() {
+        JMenuBar result = new JMenuBar();
+        JMenu fileMenu = new JMenu("File");
+        fileMenu.add(exportFigure);
+
+        result.add(fileMenu);
+
+        return result;
+    }
+
+	
+    private void exportFigure(JFreeChart chart) {
+        // TODO: use a dialog to ask for the filename
+        File svgFile = new File("example_file.svg");
+
+        // write it to file
+        try {
+            exportChartAsSVG(chart,
+                getContentPane().getBounds(), svgFile);
+
+            // TODO: notify the user the file has been saved (e.g. status bar)
+            System.out.println("Figured saved as " + svgFile.getAbsolutePath());
+
+        } catch (IOException e) {
+            System.err.println("Error saving file:\n" + e.getMessage());
+        }
+    }
+    
+    
+    void exportChartAsSVG(JFreeChart chart, Rectangle bounds, File svgFile) throws IOException {
+        // Get a DOMImplementation and create an XML document
+        DOMImplementation domImpl =
+            GenericDOMImplementation.getDOMImplementation();
+        Document document = domImpl.createDocument(null, "svg", null);
+
+        // Create an instance of the SVG Generator
+        SVGGraphics2D svgGenerator = new SVGGraphics2D(document);
+
+        // draw the chart in the SVG generator
+        chart.draw(svgGenerator, bounds);
+
+        // Write svg file
+        OutputStream outputStream = new FileOutputStream(svgFile);
+        Writer out = new OutputStreamWriter(outputStream, "UTF-8");
+        svgGenerator.stream(out, true /* use css */);
+        outputStream.flush();
+        outputStream.close();
+    }
+
+	
 
 	/**
 	 * Creates a sample dataset.
@@ -188,17 +268,64 @@ public class GraphQtdByTime extends ApplicationFrame {
 
 		 final StandardLegend legend = (StandardLegend) chart.getLegend();
 		 legend.setDisplaySeriesShapes(true);
+		 legend.setDisplaySeriesLines(true);
 
-		// get a reference to the plot for further customisation...
+
+//		// get a reference to the plot for further customisation...
 		final XYPlot plot = chart.getXYPlot();
 		plot.setBackgroundPaint(Color.lightGray);
-		 plot.setAxisOffset(new Spacer(Spacer.ABSOLUTE, 5.0, 5.0, 5.0, 5.0));
+//		// plot.setAxisOffset(new Spacer(Spacer.ABSOLUTE, 5.0, 5.0, 5.0, 5.0));
 		plot.setDomainGridlinePaint(Color.white);
 		plot.setRangeGridlinePaint(Color.white);
+		plot.setRangeGridlinesVisible(true);
+		
 
 		 final XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer();
-//		 renderer.setSeriesLinesVisible(0, false);
+//		 renderer.setSeriesLinesVisible(1, true);
+		 renderer.setBaseItemLabelsVisible(true);
+		 renderer.setDefaultLinesVisible(true);
+		 renderer.setDefaultShapesFilled(true);
+		 renderer.setDefaultShapesVisible(true);
+		 renderer.setItemLabelsVisible(true);
+//		 renderer.setSeriesItemLabelsVisible(1, true);
 //		 renderer.setSeriesShapesVisible(1, false);
+//		 
+//		 renderer.setItemLabelGenerator(new LabelGenerator(50.0));
+		 renderer.setItemLabelsVisible(null); 
+		 renderer.setSeriesItemLabelsVisible(0, true);
+		 
+		 
+		 
+
+//		 CategoryPlot plot = (CategoryPlot)chart.getPlot(); 
+//		 NumberAxis rangeAxis = (NumberAxis) plot.getRangeAxis();
+//		 rangeAxis.setRange(0, 5);  
+//		 rangeAxis.setStandardTickUnits(NumberAxis.createIntegerTickUnits());
+//		 plot.getDomainAxis().setMaximumCategoryLabelLines(5);
+//		 XYItemRenderer renderer = plot.getRenderer();
+		 
+		 renderer.setBaseItemLabelsVisible(true);  
+//		 renderer.setBaseItemLabelGenerator(new StandardCategoryItemLabelGenerator());
+		 
+//		 ValueAxis axis = plot.getRangeAxis();
+//		 axis.setTickLabelsVisible(true);
+//		 axis.setLabel("xpto");
+
+		 
+		 renderer.setBaseItemLabelFont(new Font("SansSerif", Font.PLAIN, 25)); 
+		 
+		 
+		 
+		 
+		 
+//		 plot.setToolTipGenerator(new StandardCategoryToolTipGenerator());
+			
+//		 StandardXYToolTipGenerator standardxytooltipgenerator = new StandardXYToolTipGenerator("{1} = {2}", new SimpleDateFormat("yyyy"), new DecimalFormat("0"));
+//		 renderer.setToolTipGenerator(standardxytooltipgenerator);
+
+		 
+		 
+		 
 		 plot.setRenderer(renderer);
 
 		// change the auto tick unit selection to integer units only...
@@ -240,20 +367,39 @@ public class GraphQtdByTime extends ApplicationFrame {
 	 */
 	public static void main(final String[] args) throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException {
 
-//		for (int i = 0; i < 6; i++) {
-//			GraphQtdByTime demo = new GraphQtdByTime("evolucao by tempo", i);
-//			demo.pack();
-//			RefineryUtilities.centerFrameOnScreen(demo);
-//			demo.setVisible(true);
-//			
-//		}
+		for (int i = 0; i < 6; i++) {
+			GraphQtdByTime demo = new GraphQtdByTime("evolucao by tempo", i);
+			demo.pack();
+			RefineryUtilities.centerFrameOnScreen(demo);
+			demo.setVisible(true);
+			
+		}
 		
-		GraphQtdByTime demo = new GraphQtdByTime("evolucao by tempo");
-		demo.pack();
-		RefineryUtilities.centerFrameOnScreen(demo);
-		demo.setVisible(true);
+//		GraphQtdByTime demo = new GraphQtdByTime("evolucao by tempo");
+//		demo.pack();
+//		RefineryUtilities.centerFrameOnScreen(demo);
+//		demo.setVisible(true);
 
 
 	}
 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
